263f3521927a17fee5fcccb0d0c611aae96e17a7
jeremiah <jlambright@wlapcs.org>
2017-04-11 19:07:36 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var numbers = [15, 22, 48, 37];
+var habbits = ["Respect", "Hustle","Power", "Intelligence",  ];
+var j = 0;
+
+function setup(){
+    createCanvas(600,400);
+}
+
+
+function draw(){
+         background(400,20,20);
+          fill(255,215,0);
+         ellipse(300,200,55,55);
+         ellipse(150,250,190,90);
+      
+    }
+           
+        
+
+
